84ea7a2153d100bb74e22137dcb6150efe9c6910
MOHAMED Hamda <33846477+genidevelop@users.noreply.github.com>
2025-07-16 14:18:29 +0100

    Implementing the book's exercices

---
+++ Chapter01/Part01/TenHellos.java
@@ -0,0 +1,16 @@
+public class TenHellos {
+    // Prints the message "Hello, World!" ten times to the standard output.
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+
+    }
+}
