29be93fbd7f6eeb0063bd08820206fce7ce03d5a
MOHAMED Hamda <33846477+genidevelop@users.noreply.github.com>
2025-07-16 17:36:40 +0100

    Edit RandomInt.java to generate a random number between a and b, inclusive

--- Chapter01/Part02/RandomInt.java
+++ Chapter01/Part02/RandomInt.java
@@ -1,9 +1,11 @@
 public class RandomInt {
-    // Generates a random number between 0 (inclusive) and n-1.
+    // Generates a random number between a and b , inclusive.
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
         double r = Math.random();
-        int value = (int) (r * n);
+        int value = (int) (r * Math.abs(a - b) + Math.min(a, b));
 
         System.out.println(value);
     }
