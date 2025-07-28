29541ed81b1733d5a82af5ca40454570b1c6892b
MOHAMED Hamda <33846477+genidevelop@users.noreply.github.com>
2025-07-28 10:13:28 +0100

    Enhancing the quadratic code

--- Chapter01/Part03/Quadratic.java
+++ Chapter01/Part03/Quadratic.java
@@ -1,16 +1,19 @@
 
 public class Quadratic {
     public static void main(String[] args) {
-        double b = Double.parseDouble(args[0]);
-        double c = Double.parseDouble(args[1]);
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        double c = Double.parseDouble(args[2]);
 
-        double discriminant = (b * b - 4.0 * c);
+        double discriminant = (b * b - 4.0 * a * c);
 
         if (discriminant < 0) {
             System.out.println("No real root");
+        } else if (a == 0) {
+            System.out.println("Error division by zero");
         } else {
-            System.out.println((-b + Math.sqrt(discriminant)) / 2);
-            System.out.println((-b - Math.sqrt(discriminant)) / 2);
+            System.out.println((-b + Math.sqrt(discriminant)) / (2 * a));
+            System.out.println((-b - Math.sqrt(discriminant)) / (2 * a));
         }
     }
 }
