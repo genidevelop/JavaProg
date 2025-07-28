b31a613becdef4ecceb12844251de1ad6a631726
MOHAMED Hamda <33846477+genidevelop@users.noreply.github.com>
2025-07-28 11:23:04 +0100

    Modifying TenHellos to get input from the user

--- Chapter01/Part03/TenHellos.java
+++ Chapter01/Part03/TenHellos.java
@@ -1,13 +1,17 @@
 public class TenHellos {
     public static void main(String[] args) {
-        System.out.println("1st Hello");
-        System.out.println("2nd Hello");
-        System.out.println("3rd Hello");
+        int lines = Integer.parseInt(args[0]);
 
-        int i = 4;
-        while (i <= 10) {
-            System.out.println(i + "th Hello");
-            i++;
+        for (int i = 1; i <= lines; i++) {
+            if ((i % 10 == 1 && i < 10) || (i % 10 == 1 && i >= 20) || i % 100 == 1) {
+                System.out.println(i + "st Hello");
+            } else if (i % 10 == 2 && i < 10 || i % 10 == 2 && i > 20 || i % 100 == 2) {
+                System.out.println(i + "nd Hello");
+            } else if (i % 10 == 3 && i < 10 || i % 10 == 3 && i > 20 || i % 100 == 3) {
+                System.out.println(i + "rd Hello");
+            } else {
+                System.out.println(i + "th Hello");
+            }
         }
     }
 }
