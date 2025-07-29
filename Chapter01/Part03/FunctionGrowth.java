0d0253bdabb1c0682531bde82d439529d20fad67
MOHAMED Hamda <33846477+genidevelop@users.noreply.github.com>
2025-07-29 17:04:48 +0100

    Function Growth

---
+++ Chapter01/Part03/FunctionGrowth.java
@@ -0,0 +1,20 @@
+public class FunctionGrowth {
+    public static void main(String[] args) {
+
+        System.out.println("log n \tn \tn log n\tn^2 \tn^3 \t2^n");
+        for (long i = 2; i <= 2048; i *= 2) {
+            System.out.print((int) Math.log(i));
+            System.out.print("\t");
+            System.out.print(i);
+            System.out.print("\t");
+            System.out.print((long) (i * Math.log(i)));
+            System.out.print("\t");
+            System.out.print(i * i);
+            System.out.print("\t");
+            System.out.print(i * i * i);
+            System.out.print("\t");
+            System.out.print((long) (Math.pow(2, i)));
+            System.out.println();
+        }
+    }
+}
